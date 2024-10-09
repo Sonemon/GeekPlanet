bca03d810fa63875e87293da8e23cff468bb2596
MRGL <Sonemongoo@gmail.com>
2024-10-09 05:23:22 +0300

    modal window changes for View Profile button

--- geekplanet/urls.py
+++ geekplanet/urls.py
@@ -14,9 +14,9 @@ app_name = "geekplanet"
 urlpatterns = [
     path("", MainPageView.as_view(), name="mainpage"),
     path("geeks/", UserListView.as_view(), name="user_list"),
-    path("geeks/<int:pk>", UserDetailView.as_view(), name="user_detail"),
+    path("geeks/<int:pk>/", UserDetailView.as_view(), name="user_detail"),
     path("animes/", AnimeListView.as_view(), name="anime-list"),
-    path("animes/create", AnimeCreateView.as_view(), name="anime-create"),
-    path("animes/<int:pk>", AnimeDetailView.as_view(), name="anime-detail"),
-    path("animes/<int:pk>/update", AnimeUpdateView.as_view(), name="anime-update"),
+    path("animes/create/", AnimeCreateView.as_view(), name="anime-create"),
+    path("animes/<int:pk>/", AnimeDetailView.as_view(), name="anime-detail"),
+    path("animes/<int:pk>/update/", AnimeUpdateView.as_view(), name="anime-update"),
 ]
--- static/js/user_modal.js
+++ static/js/user_modal.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const userModal = document.getElementById('userModal');
 
     userModal.addEventListener('show.bs.modal', function (event) {
-        const button = event.relatedTarget; // Кнопка, которая открыла модальное окно
-        const geekData = JSON.parse(button.getAttribute('data-geek')); // Получаем данные о пользователе
+        const button = event.relatedTarget;
+        const geekData = JSON.parse(button.getAttribute('data-geek'));
 
-        // Заполняем модальное окно данными
+        // modal content
         const modalTitle = userModal.querySelector('.modal-title');
         const modalBody = userModal.querySelector('#userModalContent');
 
@@ -14,5 +14,9 @@ document.addEventListener('DOMContentLoaded', function () {
             <p><strong>Username:</strong> ${geekData.username}</p>
             <p><strong>Friends:</strong> ${geekData.friends} friend${geekData.friends !== 1 ? 's' : ''}</p>
         `;
+
+        const viewProfileButton = document.getElementById("viewProfileButton");
+        viewProfileButton.href = `/geeks/${geekData.id}/`;
+        viewProfileButton.style.display = "inline";
     });
 });
